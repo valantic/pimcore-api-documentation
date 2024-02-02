6411ff63bb75b7657fa44f070f486e651337b509
Linus Metzler <hi@linusmetzler.me>
2024-02-02 09:39:40 +0100

    improve error handling

--- src/Controller/ApiRenderController.php
+++ src/Controller/ApiRenderController.php
@@ -20,16 +20,16 @@ class ApiRenderController extends FrontendController
             'swagger_ui_config' => [],
         ];
 
-        $apiDocs = file_get_contents(DocGeneratorCommand::DEFAULT_PATH);
-
-        if ($apiDocs === false) {
+        try {
+            $apiDocs = file_get_contents(DocGeneratorCommand::DEFAULT_PATH) ?: throw new \Exception();
+        } catch (\Throwable) {
             throw new \Exception('Docs not generated.');
         }
 
         return $this->render(
             '@NelmioApiDoc/SwaggerUi/index.html.twig',
             [
-                'swagger_data' => ['spec' => json_decode($apiDocs, true, 512, \JSON_THROW_ON_ERROR)],
+                'swagger_data' => ['spec' => json_decode($apiDocs, true, flags: \JSON_THROW_ON_ERROR)],
                 'assets_mode' => $options['assets_mode'],
                 'swagger_ui_config' => $options['swagger_ui_config'],
             ]
